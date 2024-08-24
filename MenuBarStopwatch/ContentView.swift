af257f1961f490a32c7a97bbf8271ce2c72a48a2
jtagrgh <jakob.koblinsky@gmail.com>
2024-08-24 19:47:14 -0400

    Initial Commit

---
+++ MenuBarStopwatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MenuBarStopwatch
+//
+//  Created by jakob koblinsky on 2024-08-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MenuBarStopwatch/MenuBarStopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarStopwatchApp.swift
+//  MenuBarStopwatch
+//
+//  Created by jakob koblinsky on 2024-08-24.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarStopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
